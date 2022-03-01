51592146be382b112f05cc5bd0b9844b2d7b3126
Azizbek <azizbekxm@gmail.com>
2022-03-01 16:19:02 +0500

    add AddBinary

---
+++ src/leetcodeCard/arraysAndString/ImplementStr.java
@@ -0,0 +1,25 @@
+package leetcodeCard.arraysAndString;
+
+public class ImplementStr {
+
+    //O(n) - time
+    //O(1) - space
+
+    public int strStr(String haystack, String needle) {
+        if(haystack == null || needle == null || haystack.length()<needle.length()) return -1;
+
+        int nl=needle.length();
+        if(nl == 0) return 0;
+
+
+
+        for(int i=0; i<haystack.length() - nl+1; i++){
+            if(haystack.charAt(i) != needle.charAt(0))
+                continue;
+            if(haystack.substring(i, i + nl).equals(needle))
+                return i;
+
+        }
+        return -1;
+    }
+}
---
+++ src/leetcodeCard/arraysAndString/LongestCommonPrefix.java
@@ -0,0 +1,21 @@
+package leetcodeCard.arraysAndString;
+
+public class LongestCommonPrefix {
+    //time - O(n*m)
+    //space - O(1);
+
+    public String longestCommonPrefix(String[] strs) {
+
+        if (strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
+    }
+
+}
